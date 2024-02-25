7fa04c03edf7ac309dd0ac005310f323c9363c21
madokast <578562554@qq.com>
2024-02-25 12:50:12 +0000

    own

---
+++ src/codes/own.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let s: String = String::from("hello");
+    // let s2: String = s; // move occurs because `s` has type `String`, which does not implement the `Copy` trait. value moved here
+    // use_s(s);
+    println!("{s}"); // value borrowed here after move
+                     // println!("{s2}");
+
+    let a = 1;
+    let a2 = a;
+
+    println!("{a}");
+    println!("{a2}");
+}
+
+fn use_s(s: String) {
+    println!("{s}");
+}
