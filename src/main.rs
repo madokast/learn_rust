24a7f36c476236ada9c74cc1596eeea122dfc9d7
madokast <578562554@qq.com>
2024-09-23 01:32:56 +0000

    继续学习

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,23 @@
 struct V2 {a:f64, b:f64}
 
 impl V2 {
-    fn new(x:f64, y:f64) -> Self {
+    fn new(x:f64, y:f64) -> V2 {
         V2 {a:x, b:y}
     }
-    fn length(&self) -> f64 {
+    fn length(self:&V2) -> f64 {
         (self.a * self.a + self.b *self.b).sqrt()
+    }
+}
+
+use std::fmt;
+
+impl fmt::Display for V2 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.a, self.b)
     }
 }
 
 fn main() {
     let v = V2::new(5., 12.);
-    println!("{:?} length is {}", v, v.length());
+    println!("{} is {:?} length is {}", v, v, v.length());
 }
