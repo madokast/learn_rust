5231a65ebdb9d46d0f836431a707002d1d53f645
madokast <578562554@qq.com>
2024-11-24 16:41:26 +0800

    enum

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,21 @@
-#[derive(Debug)]
-struct V2 {a:f64, b:f64}
-
-impl V2 {
-    fn new(x:f64, y:f64) -> V2 {
-        V2 {a:x, b:y}
-    }
-    fn length(self:&V2) -> f64 {
-        (self.a * self.a + self.b *self.b).sqrt()
-    }
+fn add_one(num:&mut Option<i32>) {
+    match num {
+        Some(val) => *val += 1,
+        _ => ()
+    };
 }
 
-use std::fmt;
-
-impl fmt::Display for V2 {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "({}, {})", self.a, self.b)
+fn add_one2(num:&mut Option<i32>) {
+    if let Some(val) = num {
+        *val += 1;
     }
 }
 
 fn main() {
-    let v = V2::new(5., 12.);
-    println!("{} is {:?} length is {}", v, v, v.length());
+    let mut num = Some(1);
+    println!("{:?}", num); // Some 1
+    add_one(&mut num);
+    println!("{:?}", num); // Some 2
+    add_one2(&mut num);
+    println!("{:?}", num); // Some 3
 }
